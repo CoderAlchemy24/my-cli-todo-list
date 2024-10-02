5c9f7f355c89b70b7db7316d74f4d204f00dbb1c
CodeAlchemy24 <eo.69.mzx@gmail.com>
2024-10-02 17:16:00 +0200

    modified index.js

--- index.js
+++ index.js
@@ -2,31 +2,84 @@ const fs = require('fs');
 const readline = require('readline');
 
 const rl = readline.createInterface({
-    input : process.input,
-    output : process.output
+    input : process.stdin,
+    output : process.stdout
 });
 
-const listName = 'TodoList';
-const listFilePath = 'todolist.json';
+let listName = 'listItems';
+const filePath = 'listItems.json';
 
 let listItems = [];
 
-function loadTodoList() {
-    if (fs.existsSync(listFilePath)) {
-        const data = fs.readFileSync(listFilePath);
+function loadTodos() {
+    if (fs.existsSync(filePath)) {
+        const data = fs.readFileSync(filePath);
         const savedData = JSON.parse(data);
         listName = savedData.listName;
-        listItems = savedData.listItems;
+       listItems = savedData.listItems;
     }
 }
 
-function saveTodoList() {
+function saveTodos() {
     const data = {
         listName,
-        listItems
+       listItems
     };
     fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
 }
+
+function newTodoList(name){
+    if (name) { listName = name}
+    else listName = 'Todo List';
+    saveTodos();
+    console.log(`${listName} létrehozva`);
+}
+
+function addTodo(task){
+   listItems.push({ id:listItems.length +1, task});
+    saveTodos();
+    console.log(`Feladat - ${task} hozzáadva a listához`)
+}
+
+function modifyTodo(id, newTask){
+    const todo =listItems.find(t => t.id === parseInt(id));
+    if (todo) {
+        todo.task = newTask;
+        saveTodos();
+        console.log(`Feladat módosítva: ${newTask}`);
+    } else {
+        console.log('Feladat nem található.');
+    }
+}
+
+function deleteTodo(id) {
+    const index =listItems.findIndex(t => t.id === parseInt(id));
+    if (index !== -1) {
+       listItems.splice(index, 1);
+        saveTodos();
+        console.log(`Feladat törölve: ${id}`);
+    } else {
+        console.log('Feladat nem található.');
+    }
+}
+
+// 'print' parancs: Lista és feladatok megjelenítése
+function printTodoList() {
+    if (!listName) {
+        console.log('Nincs betöltött lista.');
+    } else {
+        console.log(`Lista neve: ${listName}`);
+        if (listItems.length === 0) {
+            console.log('A lista üres.');
+        } else {
+            console.log('Feladatok:');
+           listItems.forEach(todo => {
+                console.log(`${todo.id}. ${todo.task}`);
+            });
+        }
+    }
+}
+
 
 function handleCommand(input) {
     const [command, ...args] = input.trim().split(' ');
@@ -72,7 +125,7 @@ function handleCommand(input) {
 
 // Fő program
 function main() {
-    loadTodoList();
+    loadTodos();
     rl.setPrompt('> ');
     rl.prompt();
     rl.on('line', (input) => {
